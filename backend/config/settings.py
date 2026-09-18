dc6791c948653a9827c3d588f0bdefc74debf252
Yomi <pharbeeyee16@gmail.com>
2026-09-18 14:24:05 +0100

    fixed Cloudibary

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -101,7 +101,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STORAGES = {
     "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
